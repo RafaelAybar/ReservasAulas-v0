b86ad8768c9cc3aa87ccd5fcefec82fa38193025
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2019-02-02 23:43:08 +0100

    Clase Aulas al 60%

--- src/main/java/org/iesalandalus/programacion/reservasaulas/Aulas.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/Aulas.java
@@ -1,48 +1,67 @@
 package org.iesalandalus.programacion.reservasaulas;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class Aulas implements Cloneable {
 	private static final int MAX_AULAS = 30;
 	private int numAulas = 0;
 
-	public ArrayList<Aula> coleccionAulas = new ArrayList<>();
+	public Aula[] coleccionAulas;
 
 	public Aulas() {
-
+		Aula coleccionAulas[] = new Aula[MAX_AULAS];
 	}
 
-	public Aulas(ArrayList<Aula> coleccionAulas) {
+	public Aulas(Aula[] coleccionAulas) {
 		this.coleccionAulas = coleccionAulas;
 	}
 
-	private void setAulas(Aula aula) {
+	private void setAulas(Aulas aulas) {
+		if (aulas == null) {
+			throw new IllegalArgumentException("No puede ser nulo");
+		}
+	}
+
+	public Aula[] getAulas() {
+		return coleccionAulas;
+	}
+
+	public int getNumAulas() {
+		return numAulas;
+	}
+
+	public void insertar(Aula aula) {
+		// int aulasLibres = 0;
 		if (aula == null) {
 			throw new IllegalArgumentException("No puede ser nulo");
 		} else {
-			// Comprobamos que el aula que recibimos no existe
-			for (int i = 0; i < coleccionAulas.size(); i++) {
-				if (coleccionAulas.get(i).getNombre().equals(aula.getNombre())) {
+			if (numAulas == MAX_AULAS) {
+				throw new IllegalArgumentException("No se pueden añadir más aulas");
+			}
+			// Comprobamos que el aula introducida no existe
+			for (int i = 0; i < coleccionAulas.length; i++) {
+				if (coleccionAulas[i].equals(aula)) {
 					throw new IllegalArgumentException("Esa aula ya existe");
-				} else {
-					if (numAulas == MAX_AULAS) {
-						throw new IllegalArgumentException("Ya se ha alcanzado el máximo de aulas");
-					}
-					coleccionAulas.add(aula);
-					// Incrementamos el número de aulas
-					numAulas += 1;
 				}
 			}
+			// Insertamos el aula
+			for (int i = 0; i < coleccionAulas.length; i++) {
+				if (coleccionAulas[i] == null) {
+					coleccionAulas[i] = aula;
+					break;
+				}
+			}
+			numAulas++;
 		}
 	}
 
 	// Copia profunda
-	private ArrayList<Aula> copiaProfundaAulas(ArrayList<Aula> coleccionAulas) {
+	private Aula[] copiaProfundaAulas(Aula[] coleccionAulas) {
 		// Creamos el array nuevo para copiarlo
-		ArrayList<Aula> copiaColeccionAulas;
-		copiaColeccionAulas = (ArrayList<Aula>) coleccionAulas.stream().map(Aula::new).collect(toList());
+		Aula[] copiaColeccionAulas;
+		// copiaColeccionAulas = (Aula[])
+		// coleccionAulas.stream().map(Aula::new).collect(toList());
+		copiaColeccionAulas = Stream.of(coleccionAulas).map(Aula::new).toArray(Aula[]::new);
 		return copiaColeccionAulas;
 	}
 }
